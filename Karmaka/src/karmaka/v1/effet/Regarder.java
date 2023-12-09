e80e92bdc1f6cd3fc90da778490f01a046905b27
panchotf <florianp0124@gmail.com>
2023-12-09 11:38:43 +0100

    ajout Regarder

---
+++ Karmaka/src/karmaka/v1/effet/Regarder.java
@@ -0,0 +1,44 @@
+package karmaka.v1.effet;
+
+import karmaka.v1.Carte;
+import karmaka.v1.Effet;
+import karmaka.v1.Joueur;
+import karmaka.v1.Partie;
+
+import java.util.LinkedList;
+
+public class Regarder implements Effet {
+
+
+    private LinkedList<Carte> cible;
+    private Boolean opif;
+
+    public Regarder(Boolean opif, Partie partie, LinkedList<Carte> cible) {
+        this.cible = cible;
+        this.opif = opif;
+
+    }
+
+
+    public LinkedList<Carte> getCible(){
+        return this.cible;
+    }
+
+
+
+    @Override
+    public void effet(Partie partie, Joueur joueur, int nombre, LinkedList<Carte> depart, LinkedList<Carte> arrivee, String cible, Carte carte){
+
+    }
+
+    @Override
+    public void applyEffet(Partie partie) {
+        //Carte carte = getPartie().getDefausse().getFirst();
+        //System.out.println(partie.getDefausse());
+        System.out.println(getDepart());
+        //getPartie().getDefausse().remove(carte);
+        //partie.getTest().add(carte);
+    }
+}
+{
+}
